63c10deb48ebd72e88ea6cf14f37ac953c5a727b
Tech Torrido <info.techtorrido@gmail.com>
2025-06-06 09:17:01 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -20,23 +20,29 @@ Table.displayName = "Table"
 const TableHeader = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
->(({ className, ...props }, ref) => (
-  <thead ref={ref} className={cn("[&_tr]:border-b", className)} {...props} />
-))
+>(({ className, children, ...rest }, ref) => {
+  return (
+    <thead ref={ref} className={cn("[&_tr]:border-b", className)} {...rest}>
+      {children}
+    </thead>
+  );
+});
 TableHeader.displayName = "TableHeader"
 
 const TableBody = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
->(({ className, children, ...rest }, ref) => (
-  <tbody
-    ref={ref}
-    className={cn("[&_tr:last-child]:border-0", className)}
-    {...rest}
-  >
-    {children}
-  </tbody>
-))
+>(({ className, children, ...rest }, ref) => {
+  return (
+    <tbody
+      ref={ref}
+      className={cn("[&_tr:last-child]:border-0", className)}
+      {...rest}
+    >
+      {children}
+    </tbody>
+  );
+});
 TableBody.displayName = "TableBody"
 
 const TableFooter = React.forwardRef<
@@ -57,47 +63,60 @@ TableFooter.displayName = "TableFooter"
 const TableRow = React.forwardRef<
   HTMLTableRowElement,
   React.HTMLAttributes<HTMLTableRowElement>
->(({ className, children, ...rest }, ref) => (
-  <tr
-    ref={ref}
-    className={cn(
-      "border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
-      className
-    )}
-    {...rest}
-  >{children}</tr>
-))
-TableRow.displayName = "TableRow"
+>(({ className, children, ...rest }, ref) => {
+  // Explicitly filter to ensure only valid elements are children of <tr>
+  // This helps prevent hydration errors caused by whitespace text nodes.
+  const elementChildren = React.Children.toArray(children).filter(
+    React.isValidElement
+  );
+  return (
+    <tr
+      ref={ref}
+      className={cn(
+        "border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
+        className
+      )}
+      {...rest}
+    >
+      {elementChildren}
+    </tr>
+  );
+});
+TableRow.displayName = "TableRow";
 
 const TableHead = React.forwardRef<
   HTMLTableCellElement,
   React.ThHTMLAttributes<HTMLTableCellElement>
->(({ className, children, ...rest }, ref) => (
-  <th
-    ref={ref}
-    className={cn(
-      "h-12 px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0",
-      className
-    )}
-    {...rest}
-  >
-    {children}
-  </th>
-))
+>(({ className, children, ...rest }, ref) => {
+  return (
+    <th
+      ref={ref}
+      className={cn(
+        "h-12 px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0",
+        className
+      )}
+      {...rest}
+    >
+      {children}
+    </th>
+  );
+});
 TableHead.displayName = "TableHead"
 
 const TableCell = React.forwardRef<
   HTMLTableCellElement,
   React.TdHTMLAttributes<HTMLTableCellElement>
->(({ className, children, ...rest }, ref) => (
-  <td
-    ref={ref}
-    className={cn("p-4 align-middle [&:has([role=checkbox])]:pr-0", className)}
-    {...rest}
-  >
-    {children}
-  </td>
-))
+>(({ className, children, ...rest }, ref) => {
+  return (
+    <td
+      ref={ref}
+      className={cn("p-4 align-middle [&:has([role=checkbox])]:pr-0", className)}
+      {...rest}
+    >
+      {children}
+    </td>
+  );
+});
 TableCell.displayName = "TableCell"
 
 const TableCaption = React.forwardRef<
