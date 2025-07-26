a759d7abc3483b2eb7199bca0e88a593cea68c1f
Tech Torrido <info.techtorrido@gmail.com>
2025-07-26 03:42:38 +0000

    these changes are not reflecting in the Live Websites

--- src/lib/mock-data.ts
+++ src/lib/mock-data.ts
@@ -1,4 +1,5 @@
 
+
 'use server';
 
 import type { UserProfile, LessonRequest, SummaryData, VehicleType, Course, CourseModule, CustomerRegistrationFormValues, TrainerRegistrationFormValues, ApprovalStatusType, RescheduleRequest, RescheduleRequestStatusType, UserProfileUpdateValues, TrainerSummaryData, Feedback, LessonProgressData, Referral, PayoutStatusType, QuizSet, Question, CourseModuleFormValues, QuizQuestionFormValues, FaqItem, BlogPost, SiteBanner, PromotionalPoster, FaqFormValues, BlogPostFormValues, VisualContentFormValues, FullCustomerDetailsValues } from '@/types';
