bd60967632ae08fea6064c2cc96e134e2780a7a2
Tech Torrido <info.techtorrido@gmail.com>
2025-09-06 16:21:29 +0000

    Check line 237: Make sure the return ( is properly formatted Verify all

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -4,8 +4,7 @@ import * as React from "react"
 import { cn } from "@/lib/utils"
 
 const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
-  ({ className, type, ...props }, ref) => {
-    return (
+  ({ className, type, ...props }, ref) => (
       <input
         type={type}
         className={cn(
@@ -15,8 +14,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
         ref={ref}
         {...props}
       />
-    )
-  }
+  )
 )
 Input.displayName = "Input"
 
