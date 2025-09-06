856f6e567b19a7ab4f744ba35d3da296a3256b96
Tech Torrido <info.techtorrido@gmail.com>
2025-09-06 16:17:15 +0000

    Error: ./src/app/page.tsx:240:7 Parsing ecmascript source code failed

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,9 +1,11 @@
+
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
 const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
-  ({ className, type, ...props }, ref) => (
+  ({ className, type, ...props }, ref) => {
+    return (
       <input
         type={type}
         className={cn(
@@ -13,7 +15,8 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
         ref={ref}
         {...props}
       />
-  )
+    )
+  }
 )
 Input.displayName = "Input"
 
